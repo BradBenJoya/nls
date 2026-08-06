7f7e7f33271b844b307c0f8dccdfbf9596d05bc7
BradBenJoya <bradbenjoya6@gmail.com>
2026-08-06 17:25:07 +0300

    Finished basic outputting of files

--- nls.cpp
+++ nls.cpp
@@ -1,6 +1,9 @@
-#include <iostream>
+#include <filesystem>
+#include <print>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    for (const auto& entry : std::filesystem::directory_iterator{"."}) {
+        std::println("{}", entry.path().filename().string());
+    }
     return 0;
 }
